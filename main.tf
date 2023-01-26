0d77d709a96a0b75b9459ebbfaf4a926a71c37db
up02059 <123625983+up02059@users.noreply.github.com>
2023-01-26 12:37:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,8 +44,8 @@ resource "ibm_is_subnet" "subnet2" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance1" {
-  name    = "instance1"
+resource "ibm_is_instance" "juaninstance1" {
+  name    = "juaninstance1"
   image   = var.image
   profile = var.profile
   primary_network_interface {
@@ -58,8 +58,8 @@ resource "ibm_is_instance" "instance1" {
   resource_group = data.ibm_resource_group.rg.id
 }
 
-resource "ibm_is_instance" "instance2" {
-  name    = "instance2"
+resource "ibm_is_instance" "juaninstance2" {
+  name    = "juaninstance2"
   image   = var.image
   profile = var.profile
   primary_network_interface {
